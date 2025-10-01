dcfc72e1c7740ab7e3cffd62cdaa544410d45403
williechen8399-droid <williechen8399@gmail.com>
2025-10-01 09:39:16 -0700

    Make a simple calculator

--- https:/github.com/williechen8399-droid/HelloWorld.py
+++ https:/github.com/williechen8399-droid/HelloWorld.py
@@ -4,7 +4,13 @@
 
 
 # Ask user for a name
-name  = input("What is your name?")
+name  = input("What is your name? ")
 # print hello to the user
 print ('Hello ' + name)
+print("Hello {0}".format(name))
+print( f"Hello {name}" )
+print( "Hello", name)
+message = "Hello " + name
+print( message)
 
+print("Hello","there", "Willie", sep="$", end="blah")
---
+++ https:/github.com/williechen8399-droid/SimpleCalculator.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# A simple calculator to show math and conditionals
+# Created 101 by Willie
+
+# Get two numbers from user
+number1 = float(input("First number = "))
+operation = input("What is the operation (+-*/) ")
+
+number2 = float(input('Second number = '))
+
+# Add numbers and print answer 
+if operation == "+":
+    print(f"{number1} + {number2} =", number1 + number2)
+if operation =='-':
+    print(f"{number1} - {number2} =", number1 - number2)
+if operation =="*":
+    print(f"{number1} * {number2} =", number1 * number2)
+if operation =="/":
+    print(f"{number1} / {number2} =", number1 / number2)
+if operation =="/":
+    print(f"{number1} ** {number2} =", number1 ** number2)
+if operation =="/":
+    print(f"{number1} // {number2} =", number1 // number2)
+if operation =="/":
+    print(f"{number1} % {number2} =", number1 % number2)
+if operation =="/":
+    print(f"{number1} / {number2} =", number1 / number2)
+
