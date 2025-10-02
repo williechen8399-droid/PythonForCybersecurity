7193dbea3d9ff1175caaaf2ac510c9f554337cb7
Willie Chen <williechen8399@gmail.com>
2025-10-01 22:31:41 -0700

    Update WillieChenHelloWorldQuiz.py

--- https:/github.com/williechen8399-droid/WillieChenHelloWorldQuiz.py
+++ https:/github.com/williechen8399-droid/WillieChenHelloWorldQuiz.py
@@ -1,2 +1,9 @@
+# Asking user for name
 name= input("What is your name? ")
 print("Hello",name + '.',"Have a wonderful day!" )
+
+# Asking user's age
+age= int(input("How old are you? "))
+
+futureAge = age + 2
+print(f"In 2 years, you will be {futureAge} years old.")
