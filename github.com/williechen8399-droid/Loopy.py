52bb4e685a24a852cd98bec7af6c737389c90392
williechen8399-droid <williechen8399@gmail.com>
2025-10-08 11:12:55 -0700

    Update

--- github.com/williechen8399-droid/Loopy.py
+++ github.com/williechen8399-droid/Loopy.py
@@ -7,6 +7,34 @@ def hi():
         print("Hello World")
         print(i)
 
+
+def while_example():
+    count = 0
+    while count < 6:
+        print(f"The count is {count}")
+        count = count + 1 
+def loop_array():
+    fruits = ["apple", "banana", "cherry"]
+    for fruit in fruits:
+        print(fruit)
+    
+            
+
+def nested_loop():
+    fruits = ["apple", "banana", "cherry"]
+    adj = ["shiny", "big", "tasty"]
+    for fruit in fruits:
+        for a in adj:
+            print(f"{a} {fruit}")
+    
+def loop_breal():
+    fruits = ["apple", "banana", "cherry"]
+    for fruit in fruits:
+        print(fruit)
+        if fruit == "banana":
+            break
 print("startign")
 hi()
+while_example()
+loop_array()
 print('done')
--- https:/github.com/williechen8399-droid/pinger1.py
+++ https:/github.com/williechen8399-droid/pinger1.py
@@ -8,18 +8,27 @@ import platform
 # Assign IP to variable
 ip_addr ="127.0.0.1"
 
+#Assign IP to variable 
+
+ip_prefix = '192.168.0.'
+
+# Start Loop
+for final_octect in range(254):
+    # Build IP address
+    ip_addr = ip_prefix + str(final_octect + 1)
+
+
 # Find current OS
 current_os = platform.system().lower()
 # If Windows
 if current_os == "windows":
-    ping_cmd = ""
+    ping_cmd = "ping -n 1 -w 2" + ip_addr + " > nul"
 else:
-    ping_cmd = "ping -c 1 -w 2" + ip_addr + " > nul"
+    ping_cmd = "ping -c 1 -w 2" + ip_addr + " > /dev/null 2>&1"
 
 print(ping_cmd)
 # Build ping command
 
-print(ping_cmd)
 
 # Execuate ping command
 exit_code = os.system(ping_cmd)
