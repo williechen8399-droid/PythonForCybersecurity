ecd017fc2a1787381db75f2a57879c22037ab465
williechen8399-droid <williechen8399@gmail.com>
2025-10-08 11:30:38 -0700

    Created conditionals quiz

---
+++ github.com/williechen8399-droid/Willie_Chen_Conditional_Quiz.py
@@ -0,0 +1,14 @@
+# Get user inpuy
+
+userInput= input("Was today a gooday? (yes/no): ")
+
+# Convert input to lowercase
+userInput = userInput.lower()
+
+# Condition statement
+if userInput == "yes":
+    print("That's great!")
+elif userInput == "no":
+    print("Don't worry, It will get better!")
+else: 
+    print("Please answer 'yes' or 'no'.")
