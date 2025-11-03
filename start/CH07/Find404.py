90b7e407314881c3274099a07ebf8b00b1897bc1
Willie Chen <williechen8399@gmail.com>
2025-11-03 09:12:55 -0800

    Delete start/CH07 directory

--- start/CH07/Find404.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env python3
-# Script that scans web server logs for 404 errors
-# By 
--- start/CH07/FindClients.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-# Script that scans web server logs for client addresses
-# Use RegEx to find and report on most frequent users
-# By 
