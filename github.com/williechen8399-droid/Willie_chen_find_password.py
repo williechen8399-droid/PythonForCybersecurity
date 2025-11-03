7a0587b2e614fe1d400f4031adb9eba404dc18e5
Willie Chen <Williechen8399@gmail.com>
2025-11-02 23:34:51 -0800

    making find password

---
+++ github.com/williechen8399-droid/Willie_chen_find_password.py
@@ -0,0 +1,38 @@
+# Find the password for the user 
+
+import crypt
+
+# Part 1: Load the user and find hashes
+users = {}
+with open("shadow") as f:
+    for line in f:
+        parts = line.split(":")
+        if len(parts) > 1 and parts[1].strip() not in ("", "!", "*"):
+            name = parts[0]
+            fullHash = parts[1].strip()
+            users[name] = fullHash
+
+
+# Part 2: We need to get salt from hash
+def getSalt(h):
+    if h.startswith("$"):
+        spot = h.find("$", 3)
+        if spot != -1:
+            return h[:spot+1]
+    return h[:2]  
+
+salts = {name: getSalt(h) for name, h in users.items()}     
+
+
+#Part 3: test out the passwords
+for guess in open("top-100.txt"):   
+    guess = guess.strip()
+    if not guess:
+        continue
+    for name, fullHash in list(users.items()):
+        test = crypt.crypt(guess, salts[name])
+        if test == fullHash:
+            print("FOUND:", name, "=", guess)
+            users.pop(name)
+    if not users:
+        break
