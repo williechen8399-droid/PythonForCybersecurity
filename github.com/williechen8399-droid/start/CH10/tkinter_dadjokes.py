54db0263643de31318e96bbbb860f8f54f0cb3cb
Willie Chen <williechen8399@gmail.com>
2025-11-03 09:40:25 -0800

    Add files via upload

---
+++ github.com/williechen8399-droid/start/CH10/tkinter_dadjokes.py
@@ -0,0 +1,16 @@
+# third tkinter script
+# Get people in space
+# Create by 
+
+# Import tkinter
+
+# Functions
+
+# Create the GUI main window
+
+# Create font object
+# Set wraplength to 25 characters
+
+# Add widgets
+
+# Enter the main event loop
